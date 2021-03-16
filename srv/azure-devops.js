2cea6f352fb452b94eb7a3120f57a1ca4cd74a4b
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2021-03-16 19:50:22 +0100

    Dynamic WIQL-queries from Azure DevOps

--- srv/azure-devops.js
+++ srv/azure-devops.js
@@ -1,8 +1,41 @@
 module.exports = cds.service.impl(async function () {
   const azdev = require("azure-devops-node-api");
+  const { SelectBuilder } = require("@sap/cds-runtime/lib/db/sql-builder");
 
   if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
+  }
+
+  function getSubstringBetween(string, begin, end) {
+    const substringBetween = string.substring(
+      string.lastIndexOf(begin) + begin.length,
+      string.lastIndexOf(end)
+    );
+
+    return substringBetween;
+  }
+
+  function getWhereClause(substring) {
+    return getSubstringBetween(substring, "WHERE", "ORDER BY");
+  }
+
+  function transformToWIQL(substring) {
+    const substrings = substring.split(" ");
+
+    const WIQL = substrings
+      .filter((str) => str.length > 0)
+      .reduce((str, substr) => {
+        const next =
+          substr.length > 3 &&
+          !(substr === substr.toUpperCase()) &&
+          !substr.startsWith("'")
+            ? `[System.${substr}]`
+            : substr;
+        const result = str.concat(` ${next}`);
+        return result;
+      }, "");
+
+    return WIQL;
   }
 
   // your collection url
@@ -16,15 +49,28 @@ module.exports = cds.service.impl(async function () {
   let workItemAPI = await connection.getWorkItemTrackingApi();
 
   this.on("READ", Items, async (req) => {
-    const userID = req.query.SELECT.where[2].val;
+    const { where } = req?.query?.SELECT;
+    const userID = where && where[2]?.val;
+    const selectBuilder = new SelectBuilder(req.query);
+    const SQLString = selectBuilder.build();
+
+    const whereClauseGeneric = getWhereClause(SQLString.sql);
+
+    const whereClause = SQLString.values.reduce((string, value) => {
+      return string.replace("?", `'${value}'`);
+    }, whereClauseGeneric);
+
+    const whereClauseWIQL = transformToWIQL(whereClause);
+
+    if (!userID) return [];
 
     const workItemsByWIQL = await workItemAPI.queryByWiql({
-      query: `SELECT [System.Id],[System.WorkItemType],[System.Title],[System.AssignedTo],[System.State],[System.Tags] FROM WorkItems WHERE [System.TeamProject] = 'IOT Projekte' AND [System.WorkItemType] <> '' AND [System.AssignedTo] = '${userID}'`,
+      query: `SELECT [System.Id],[System.WorkItemType],[System.Title],[System.AssignedTo],[System.State],[System.Tags] FROM WorkItems WHERE [System.TeamProject] = 'IOT Projekte' AND [System.WorkItemType] <> '' AND ${whereClauseWIQL}`,
     });
 
     const ids = workItemsByWIQL.workItems.map(({ id }) => id);
 
-    const wiDetails = await workItemAPI.getWorkItems(ids);
+    const wiDetails = (await workItemAPI.getWorkItems(ids)) || [];
     // https://dev.azure.com/{organization}/{project}/_apis/wit/workitems?ids={ids}&api-version=6.0
 
     const results = wiDetails
@@ -34,8 +80,8 @@ module.exports = cds.service.impl(async function () {
         AssignedTo: item["System.AssignedTo"].uniqueName,
         AssignedToName: item["System.AssignedTo"].displayName,
         ChangedDate: item["System.ChangedDate"],
-        CompletedWork: item["Microsoft.VSTS.Scheduling.CompletedWork"],
         CreatedDate: item["System.CreatedDate"],
+        CompletedWork: item["Microsoft.VSTS.Scheduling.CompletedWork"],
         OriginalEstimate: item["Microsoft.VSTS.Scheduling.OriginalEstimate"],
         Reason: item["System.Reason"],
         State: item["System.State"],
