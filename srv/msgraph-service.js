4228717646f779d38921c87d5a46d48ad6dcd131
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2022-06-09 23:59:35 +0200

    Remove overlap of CLIENT_ID

--- srv/msgraph-service.js
+++ srv/msgraph-service.js
@@ -2,6 +2,12 @@
 // const { ExpressionBuilder } = require("@sap/cds-runtime/lib/db/sql-builder");
 const ExpressionBuilder = require("@sap/cds/libx/_runtime/db/sql-builder/ExpressionBuilder");
 
+// MSGraph connection is inspired by: https://docs.microsoft.com/en-us/azure/active-directory/develop/tutorial-v2-nodejs-console
+// const auth = require("./msgraph/auth");
+// const fetch = require("./msgraph/fetch");
+const auth = {};
+const fetch = {};
+
 function parseQueryParams(select) {
   const { values } = new ExpressionBuilder(select.where).build();
 
@@ -18,13 +24,13 @@ function parseQueryParams(select) {
 }
 
 module.exports = async function (srv) {
-  const MSGraphSrv = {};
-  // const MSGraphSrv = await cdsapi.connect.to("MicrosoftGraphIOTGmbH");
-
   srv.on("READ", "Events", async (req) => {
     const user = process.env.NODE_ENV
       ? req.user.id
       : "daniel.mextorf@iot-online.de";
+
+    // here we get an access token
+    const authResponse = await auth.getToken(auth.tokenRequest);
 
     const query = parseQueryParams(req.query.SELECT);
     const queryString = Object.entries(query)
@@ -40,19 +46,26 @@ module.exports = async function (srv) {
       "sensitivity",
     ];
 
-    const { value } = await MSGraphSrv.run({
-      url: `/v1.0/users/${user}/calendarview?${queryString}&$top=1000&$select=${selectFields.join(
+    var { value } = await fetch.callApi(
+      `${
+        auth.apiConfig.uri
+      }/${user}/calendarview?${queryString}&$top=1000&$select=${selectFields.join(
         ","
       )}`,
-    });
+      authResponse.accessToken
+    );
 
     return value.map((val) => ({ ...val, id: encodeURIComponent(val.id) }));
   });
 
   srv.on("READ", "Users", async () => {
-    const { value } = await MSGraphSrv.run({
-      url: `/v1.0/users`,
-    });
+    // here we get an access token
+    const authResponse = await auth.getToken(auth.tokenRequest);
+
+    const { value } = await fetch.callApi(
+      auth.apiConfig.uri,
+      authResponse.accessToken
+    );
 
     return value.map(({ id, ...usr }) => ({
       ID: id,
