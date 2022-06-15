80806b751ecf6ee38344c3944d88c6c76ec57ee4
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2022-06-15 14:54:16 +0200

    feat: Integrate MSAL Authentication

--- srv/server.js
+++ srv/server.js
@@ -1,28 +1,31 @@
 "use strict";
 
 const cds = require("@sap/cds");
-const express = require("express");
+// const express = require("express");
 const proxy = require("@sap/cds-odata-v2-adapter-proxy");
-const { auth, requiresAuth } = require("express-openid-connect");
+const msalAuth = require("cds-msal-auth/auth");
+// const { auth, requiresAuth } = require("express-openid-connect");
 
 require("dotenv").config();
-const auth0Audience = process.env.AUTH0_AUDIENCE;
+// const auth0Audience = process.env.AUTH0_AUDIENCE;
 
-const config = {
-  authRequired: false, // deactivate auth for all routes
-  auth0Logout: true, // logout from IdP
-  authorizationParams: {
-    // required to retrieve JWT including permissions (our roles)
-    response_type: "code",
-    scope: "openid",
-    audience: auth0Audience,
-  },
-};
+// const config = {
+//   authRequired: false, // deactivate auth for all routes
+//   auth0Logout: true, // logout from IdP
+//   authorizationParams: {
+//     // required to retrieve JWT including permissions (our roles)
+//     response_type: "code",
+//     scope: "openid",
+//     audience: auth0Audience,
+//   },
+// };
 
 cds.on("bootstrap", (app) => {
   // initialize openid-connect with auth0 configuration
-  app.use(auth(config));
+  // app.use(auth(config));
   app.use(proxy());
+
+  msalAuth(app);
 
   //test
 
@@ -31,14 +34,14 @@ cds.on("bootstrap", (app) => {
   // cf. https://blogs.sap.com/2021/08/06/sap-cap-with-social-login-and-rbac-using-auth0/
   // app.use("/app", requiresAuth(), express.static(__dirname + "/../app"));
 
-  app.use(
-    "/fiori.html",
-    requiresAuth(),
-    express.static(__dirname + "../app/fiori.html")
-  );
+  // app.use(
+  //   "/fiori.html",
+  //   requiresAuth(),
+  //   express.static(__dirname + "../app/fiori.html")
+  // );
 
-  // for example, serve static resources incl. index.html
-  app.use(express.static(__dirname + "/app2"));
+  // // for example, serve static resources incl. index.html
+  // app.use(express.static(__dirname + "/app2"));
 });
 
 module.exports = cds.server;
