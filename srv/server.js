09bd76a28f453f5be2bcf8b49ed47833e22c534c
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2022-06-22 19:28:27 +0200

    fix: Update .env-names

--- srv/server.js
+++ srv/server.js
@@ -4,6 +4,8 @@ const cds = require("@sap/cds");
 const proxy = require("@sap/cds-odata-v2-adapter-proxy");
 const msalAuth = require("cds-msal-auth/auth");
 
+require("dotenv").config();
+
 cds.on("bootstrap", (app) => {
   app.use(proxy());
 
