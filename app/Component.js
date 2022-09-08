3c368bcc085b76ccc2ec0a92fa8b108e01b7a01a
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2022-09-08 11:30:34 +0200

    fix: repair data selection for fiori elements workitems

--- app/Component.js
+++ app/Component.js
@@ -20,23 +20,22 @@ sap.ui.define(
        * @return {string} css class, either 'sapUiSizeCompact' or 'sapUiSizeCozy' - or an empty string if no css class should be set
        */
       getContentDensityClass() {
-        // if (this._sContentDensityClass === undefined) {
-        //   // check whether FLP has already set the content density class; do nothing in this case
-        //   if (
-        //     document.body.classList.contains("sapUiSizeCozy") ||
-        //     document.body.classList.contains("sapUiSizeCompact")
-        //   ) {
-        //     this._sContentDensityClass = "";
-        //   } else if (!Device.support.touch) {
-        //     // apply "compact" mode if touch is not supported
-        //     this._sContentDensityClass = "sapUiSizeCompact";
-        //   } else {
-        //     // "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
-        //     this._sContentDensityClass = "sapUiSizeCozy";
-        //   }
-        // }
-        // return this._sContentDensityClass;
-        return "sapUiSizeCozy";
+        if (this._sContentDensityClass === undefined) {
+          // check whether FLP has already set the content density class; do nothing in this case
+          if (
+            document.body.classList.contains("sapUiSizeCozy") ||
+            document.body.classList.contains("sapUiSizeCompact")
+          ) {
+            this._sContentDensityClass = "";
+          } else if (!Device.support.touch) {
+            // apply "compact" mode if touch is not supported
+            this._sContentDensityClass = "sapUiSizeCompact";
+          } else {
+            // "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
+            this._sContentDensityClass = "sapUiSizeCozy";
+          }
+        }
+        return this._sContentDensityClass;
       },
     })
 );
