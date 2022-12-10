a7071193e8b083951f9cd66d99c807e39a34798c
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2022-12-10 14:28:36 +0100

    feat: add header

--- app/controller/Home.controller.js
+++ app/controller/Home.controller.js
@@ -1,45 +1,57 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) =>
-  Controller.extend("iot.planner.controller.Home", {
-    navTo(event, pattern, target, deepRoute) {
-      let deepRoutingConfig = {};
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"],
+  (Controller, History) =>
+    Controller.extend("iot.planner.controller.Home", {
+      onNavBack() {
+        const sPreviousHash = History.getInstance().getPreviousHash();
 
-      if (target && deepRoute) {
-        deepRoutingConfig = {
-          [target]: { route: deepRoute },
-        };
-      }
-      this.getOwnerComponent()
-        .getRouter()
-        .navTo(pattern, {}, deepRoutingConfig);
-    },
+        if (sPreviousHash !== undefined) {
+          history.go(-1);
+        } else {
+          this.getRouter().navTo("masterDetail", {}, true);
+        }
+      },
 
-    navToMasterDetail() {
-      // navTo($event, "trackViaCalendar&/calendar/singleEntry");
-      this.getOwnerComponent()
-        .getRouter()
-        .navTo(
-          "trackViaCalendar",
-          {},
-          {
-            spc: {
-              route: "masterDetail",
-            },
-          }
-        );
-    },
+      navTo(event, pattern, target, deepRoute) {
+        let deepRoutingConfig = {};
 
-    navToAnalytics() {
-      this.getOwnerComponent()
-        .getRouter()
-        .navTo(
-          "manageCategories",
-          {},
-          {
-            manageCategories: {
-              route: "graph",
-            },
-          }
-        );
-    },
-  })
+        if (target && deepRoute) {
+          deepRoutingConfig = {
+            [target]: { route: deepRoute },
+          };
+        }
+        this.getOwnerComponent()
+          .getRouter()
+          .navTo(pattern, {}, deepRoutingConfig);
+      },
+
+      navToMasterDetail() {
+        // navTo($event, "trackViaCalendar&/calendar/singleEntry");
+        this.getOwnerComponent()
+          .getRouter()
+          .navTo(
+            "trackViaCalendar",
+            {},
+            {
+              spc: {
+                route: "masterDetail",
+              },
+            }
+          );
+      },
+
+      navToAnalytics() {
+        this.getOwnerComponent()
+          .getRouter()
+          .navTo(
+            "manageCategories",
+            {},
+            {
+              manageCategories: {
+                route: "graph",
+              },
+            }
+          );
+      },
+    })
 );
