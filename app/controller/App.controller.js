c91cdbb3b01c15e1d77eb12612f1016937645f3b
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2022-12-22 11:09:10 +0100

    chore: set app-height

--- app/controller/App.controller.js
+++ app/controller/App.controller.js
@@ -3,11 +3,23 @@ sap.ui.define(
     // "../Authentication",
     "./BaseController",
     "sap/ui/core/routing/History",
-    "iot/BackendJSONModel",
+    "sap/ui/model/json/JSONModel",
     "errorhandler/ErrorHandler",
   ],
-  (BaseController, History, BackendJSONModel, ErrorHandler) =>
+  (BaseController, History, JSONModel, ErrorHandler) =>
     BaseController.extend("iot.planner.controller.App", {
+      onInit() {
+        const headerToolbarHeight = 49;
+        const heightWithoutHeaderToolbar =
+          $(document).height() - headerToolbarHeight;
+        this.setModel(
+          new JSONModel({
+            heightWithoutHeaderToolbar: `${heightWithoutHeaderToolbar}px`,
+          }),
+          "viewModel"
+        );
+      },
+
       login() {
         return this.getOwnerComponent().login();
       },
